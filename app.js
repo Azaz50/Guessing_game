c327cc4f18402a164604e991c8a203e9faa2beb8
Azaz50 <azazmohammad14@gmail.com>
2023-08-28 20:39:58 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const max = prompt("enter the max number");
+const random = Math.floor(Math.random()*max)+1;
+
+let guess = prompt("guess the number");
+
+while(true){
+    if (guess == "quit"){
+        console.log("user quit");
+        break;
+    }
+    if (guess == random){
+        console.log("Congrats, you guess the right number", random);
+        break;
+    }
+    else if(guess < random){
+        guess = prompt("Hints: your guess was too small");
+    }else{
+        guess = prompt("Hints: Your guess was too large");
+    }
+}
